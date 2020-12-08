7aff2a021874c3c5b3ac35c3d318deb0560dab5c
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2020-12-08 19:51:57 +0100

    Open works but image doesn't appear.

--- src/sdl/main.c
+++ src/sdl/main.c
@@ -13,7 +13,7 @@ typedef struct UserInterface
     GtkWindow *window;
 
     // Auxilliary top-levels
-    GtkFileChooserDialog *file_chooser;
+    // GtkFileChooserDialog *file_chooser;
 
     // Top menu
     BannerMenu banner_menu;
@@ -24,20 +24,37 @@ typedef struct UserInterface
 
 gboolean on_input_image_button_release_event(UserInterface *ui)
 {
-    g_print("coucou 1 %p", ui);
+    g_print("coucou sale %p\n", ui);
     return FALSE;
 }
 
 gboolean on_open_activate(UserInterface *ui)
 {
-    if (gtk_dialog_run (GTK_DIALOG (ui->file_chooser)) == GTK_RESPONSE_ACCEPT)
-    {
-        char *filename;
+    GtkWidget *dialog = gtk_file_chooser_dialog_new(
+            "Open File", ui->window, 
+            GTK_FILE_CHOOSER_ACTION_OPEN,
+            "Cancel", GTK_RESPONSE_CANCEL,
+            "Open", GTK_RESPONSE_ACCEPT,
+            NULL);
 
-        filename = gtk_file_chooser_get_filename(
-                GTK_FILE_CHOOSER(ui->file_chooser));
-        gtk_image_set_from_file(ui->input_image, filename);
+    const char *filename;
+    switch(gtk_dialog_run(GTK_DIALOG(dialog)))
+    {
+        case GTK_RESPONSE_ACCEPT:
+            filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+            ui->input_image = GTK_IMAGE(gtk_image_new_from_file(filename));
+            break;
+        case GTK_RESPONSE_CANCEL:
+            g_print("cancelled\n");
+            break;
+        default:
+            g_print("fck u\n");
+            break;
     }
+
+    g_print("coucou %p\n", ui->input_image);
+
+    gtk_widget_destroy(dialog);
 
     return FALSE;
 }
@@ -61,12 +78,7 @@ int main()
 
     GtkWindow *window =
         GTK_WINDOW(gtk_builder_get_object(builder, "org.gtk.ocr"));
-    GtkFileChooserDialog *file_chooser =
-        GTK_FILE_CHOOSER_DIALOG(gtk_file_chooser_dialog_new(
-                    "Open File", window, GTK_FILE_CHOOSER_ACTION_OPEN,
-                    "Cancel", GTK_RESPONSE_CANCEL,
-                    "Open", GTK_RESPONSE_ACCEPT,
-                    NULL));
+    // GtkFileChooserDialog *file_chooser =
     GtkMenuBar *menu =
         GTK_MENU_BAR(gtk_builder_get_object(builder, "menu"));
     GtkMenuItem *open =
@@ -78,7 +90,7 @@ int main()
     {
         .window = window,
 
-        .file_chooser = file_chooser,
+        // .file_chooser = file_chooser,
 
         .banner_menu =
         {
@@ -92,8 +104,6 @@ int main()
     // Connects event handlers.
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(open, "activate", G_CALLBACK(on_open_activate), &ui);
-    g_signal_connect(input_image, "button-release-event",
-            G_CALLBACK(on_input_image_button_release_event), &ui);
 
     gtk_main();
 
@@ -102,142 +112,3 @@ int main()
 
 
 
-/* Legacy CLI version
- * int main(int argc, char *argv[])
- * {
- *     if (argc < 2)
- *     {
- *         errx(0, "A file has to be specified.");
- *     }
- *
- *     SDL_Surface* image_surface = NULL;
- *     SDL_Surface* screen_surface = NULL;
- *
- *     init_sdl();
- *
- *     for(int i = 1; i < argc; i++)
- *     {
- *         if(argv[i][0] == '-' && argv[i][1] != '-')
- *         {
- *             switch(argv[i][1])
- *             {
- *                 case 'i':
- *                     if(i < argc-1)
- *                     {
- *                         image_surface = load_image(argv[i+1]);
- *                         i++;
- *                     }
- *                     else
- *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
- *                     break;
- *                 case 'o':
- *                     if(i < argc-1)
- *                     {
- *                         save_image(image_surface, argv[i+1]);
- *                         i++;
- *                     }
- *                     else
- *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
- *                     break;
- *                 case 'g':
- *                     if(image_surface != NULL)
- *                         greyscale(image_surface);
- *                     else
- *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
- *                     break;
- *                 case 'v':
- *                     if(image_surface != NULL)
- *                     {
- *                         greyscale(image_surface);
- *                         image_surface = Otsu_method(image_surface, 0);
- *                     }
- *                     else
- *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
- *                     break;
- *                 case 'r':
- *                     if(image_surface != NULL)
- *                         image_surface = auto_rotate(image_surface);
- *                     else
- *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
- *                     break;
- *                 case 'b':
- *                     if(image_surface != NULL)
- *                         image_surface = convolute(image_surface, gaussian_blur,
- *                                 ARRAYLEN(gaussian_blur));
- *                     else
- *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
- *                     break;
- *                 case 'e':
- *                     if(image_surface != NULL)
- *                         image_surface = convolute(image_surface, edge_detection,
- *                                 ARRAYLEN(edge_detection));
- *                     else
- *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
-*                     break;
-*                 case 'd':
-*                     if(image_surface != NULL)
-    *                         screen_surface = display_image(image_surface);
-    *                     else
-    *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
-    *                     break;
-    *                 case 's':
-*                     if(image_surface != NULL)
-    *                         SegmentationTest(image_surface);
-    *                     else
-    *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
-    *                     break;
-    *                 case 'c':
-*                     if(image_surface != NULL)
-    *                         fill_edges(image_surface, 0, 255);
-    *                     else
-    *                         errx(0, "-%c: Incorrect usage, check man.", argv[i][1]);
-    *                     break;
-    *                 default:
-    *                     errx(1, "-%c: Invalid command, check man.", argv[i][1]);
-    *             }
-    *         }
-    *
-    * //        if(compare_strings(argv[i], "--dothething") == 1)
-    * //        {
-    * //            if(i < argc-1)
-    * //            {
-    * //                image_surface = load_image(argv[i+1]);
-    * //                // Otsu (-v)
-    * //                greyscale(image_surface);
-    * //                image_surface = Otsu_method(image_surface, 0);
-    * //                // Clean (-c)
-    * //                fill_edges(image_surface, 0, 255);
-    * //                // Auto-rotate (-r)
-    * //                image_surface = Otsu_method(image_surface, 255);
-    * //                image_surface = auto_rotate(image_surface);
-    * //                image_surface = Otsu_method(image_surface, 0);
-    * //                // Gaussian blur (-b)
-    * //                image_surface = convolute(image_surface, gaussian_blur,
-    * //                        ARRAYLEN(gaussian_blur));
-    * //                fill_edges(image_surface, 0, 255);
-    * //                i++;
-    * //            }
-    * //            else
-    * //                errx(0, "--dothething: Incorrect usage, check man.");
-    * //        }
-    * //        if(compare_strings(argv[i], "--finishthething") == 1)
-    * //        {
-    * //            if(image_surface != NULL)
-    * //            {
-    * //                SegmentationTest(image_surface);
-    * //                save_image(image_surface, "eye_t.bmp");
-    * //                wait_for_keypressed();
-    * //            }
-    * //            else
-    * //                errx(0, "--finishthething Incorrect usage, check man.");
-    * //        }
-    *     }
-    *
-*     if(image_surface != NULL)
-    *         SDL_FreeSurface(image_surface);
-*     if(screen_surface != NULL)
-    *         SDL_FreeSurface(screen_surface);
-    *
-    *     return 0;
-    * }
-    */
